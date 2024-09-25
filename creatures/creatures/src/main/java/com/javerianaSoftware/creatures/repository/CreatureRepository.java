38e4e679976d6bdeb338191669a7ce7e06f23ff5
andreslopez <damodmo1110@gmail.com>
2024-09-25 00:52:31 -0500

    prueba 2, es la carpeta llamada p2 si sirve, probado en postman

--- creatures/creatures/src/main/java/com/javerianaSoftware/creatures/repository/CreatureRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javerianaSoftware.creatures.repository;
-
-import com.javerianaSoftware.creatures.model.Creature;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CreatureRepository extends JpaRepository<Creature, Long> {
-}
